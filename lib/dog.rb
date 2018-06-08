acbe6aeb79983df1d1e989f7aaa1bab7fe276dbe
Allora Sisnroy <allsis@icloud.com>
2018-06-08 19:25:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,4 +3,16 @@ class Dog
  def name 
    @name 
  end
- def name= ()
+ 
+ def name = (name)
+   @name = name
+ end
+ 
+ def breed 
+   @breed
+ end
+ 
+ def breed = (breed)
+   @name = name
+ end
+ end
